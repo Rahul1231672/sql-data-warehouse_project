55ce2fc5861b4fba6c0b596fde869be38b301bf8
Rahul Kumar <xyz123167@gmail.com>
2025-09-27 00:49:04 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,19 @@
+/*
+--------------------------------------------------------------------------------------------------------------------------------------
+CREATE DATABASE AND SCHEMAS
+--------------------------------------------------------------------------------------------------------------------------------------
+SCRIPT PURPOSE:
+this script create new database after checking if not exists. then also creates three different schema for all three layers
+
+
+WARNING:
+Running this script will drop the entire 'datawarehouse' database if exists
+All data in database will be deleted permanently. proceed with caution and ensure you have proper backups before running the script
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 --DROP IF DATABASE ALREADY EXISTS
 DROP DATABASE IF EXISTS DataWarehouse;
 --CREATE DATABASE

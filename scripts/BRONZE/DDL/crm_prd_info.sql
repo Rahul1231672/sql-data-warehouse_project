07af1ee704649282ca933ba05909f2a5acae183a
Rahul Kumar <xyz123167@gmail.com>
2025-09-27 04:13:59 +0530

    Create crm_prd_info.sql

---
+++ scripts/BRONZE/DDL/crm_prd_info.sql
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------
+DDL SCRIPT
+-------------------------------------------------------------------------
+  SCRIPT PURPOSE:
+
+
+-------------------------------------------------------------------------
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+prd_id int,
+prd_key varchar(50),
+prd_nm varchar(50),
+prd_cost int,
+prd_line varchar(50),
+prd_start_dt date,
+prd_end_dt date
+);
